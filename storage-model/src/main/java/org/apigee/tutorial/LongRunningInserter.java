2277d0e6307e17bf4ef21d5e79c25453ed258ef9
zznate <zznate.m@gmail.com>
2012-02-27 16:45:40 -0800

    added impl detail to LRI

--- storage-model/src/main/java/org/apigee/tutorial/LongRunningInserter.java
+++ storage-model/src/main/java/org/apigee/tutorial/LongRunningInserter.java
@@ -1,8 +1,25 @@
 package org.apigee.tutorial;
 
+import me.prettyprint.cassandra.model.BasicColumnFamilyDefinition;
+import me.prettyprint.cassandra.serializers.LongSerializer;
+import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.cassandra.service.ThriftCfDef;
+import me.prettyprint.cassandra.utils.TimeUUIDUtils;
+import me.prettyprint.hector.api.beans.HColumn;
+import me.prettyprint.hector.api.ddl.ColumnFamilyDefinition;
+import me.prettyprint.hector.api.ddl.ComparatorType;
+import me.prettyprint.hector.api.factory.HFactory;
+import me.prettyprint.hector.api.mutation.Mutator;
+import org.apache.commons.lang.math.RandomUtils;
+import org.apigee.tutorial.common.SchemaUtils;
 import org.apigee.tutorial.common.TutorialBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.Callable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
 
 /**
  * Low-intensity, long-running inserts. Use in combination with command-line
@@ -21,29 +38,91 @@ import java.util.concurrent.Callable;
  * DataStax OpsCenter:
  * TODO add screen instructions
  *
+ * mvn -e exec:java -Dexec.mainClass="org.apigee.tutorial.TimeseriesInserter"
  * @author zznate
  */
 public class LongRunningInserter extends TutorialBase {
+  private static Logger log = LoggerFactory.getLogger(LongRunningInserter.class);
+
+  public static final String CF_LONG_RUNNING_INSERT = "LongRunningInsert";
+
+  private static ExecutorService exec;
+  private static Random random = new Random();
 
   public static void main(String[] args ) {
-    // executor runs single insert-then-sleep
-    // - sleep is a long enough window to exploit flush semantics
+    init();
+    maybeCreateSchema();
+    long startTime = System.currentTimeMillis();
+    init();
+    exec = Executors.newFixedThreadPool(3);
+
+    try {
+
+      List<Future<Integer>> futures = new ArrayList<Future<Integer>>();
+
+      // request 10 invocation of RowInserter
+      // each invocation creates 5 rows, thus we have 50 rows of 1000 columns each
+      for (int x = 0; x < 100; x++) {
+        futures.add(exec.submit(new LongRunningInserter().new RowInserter()));
+        TimeUnit.SECONDS.sleep(random.nextInt(4));
+      }
+
+      int total = 0;
+      try {
+        for (Future<Integer> f : futures) {
+          total += f.get().intValue();
+        }
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+
+    } catch (Exception e) {
+      log.error("Could not locate file",e);
+    } finally {
+      exec.shutdown();
+    }
 
   }
 
-  protected void maybeCreateSchema() {
-    // TODO
+  protected static void maybeCreateSchema() {
+    BasicColumnFamilyDefinition columnFamilyDefinition = new BasicColumnFamilyDefinition();
+    columnFamilyDefinition.setKeyspaceName(SchemaUtils.TUTORIAL_KEYSPACE_NAME);
+    columnFamilyDefinition.setName(CF_LONG_RUNNING_INSERT);
+    columnFamilyDefinition.setComparatorType(ComparatorType.LONGTYPE);
+    columnFamilyDefinition.setDefaultValidationClass(ComparatorType.LONGTYPE.getClassName());
+    columnFamilyDefinition.setKeyValidationClass(ComparatorType.UTF8TYPE.getClassName());
+    ColumnFamilyDefinition cfDef = new ThriftCfDef(columnFamilyDefinition);
+    schemaUtils.maybeCreate(cfDef);
   }
 
-  private void doInsert() {
 
-  }
-
-  class Inserter implements Callable {
+  class RowInserter implements Callable {
 
     @Override
     public Object call() throws Exception {
-      return null;
+      Mutator<String> mutator = HFactory.createMutator(tutorialKeyspace, StringSerializer.get());
+      int count = 0;
+      String myKey = TimeUUIDUtils.getTimeUUID(tutorialKeyspace.createClock()).toString();
+      for (int x=0; x<5000; x++) {
+
+        // assemble the insertions
+        mutator.addInsertion(myKey,CF_LONG_RUNNING_INSERT, buildColumnFor(x));
+        if ( x % 1000 == 0 ) {
+          myKey = TimeUUIDUtils.getTimeUUID(tutorialKeyspace.createClock()).toString();
+          count++;
+          TimeUnit.SECONDS.sleep(random.nextInt(4));
+        }
+
+      }
+      mutator.execute();
+
+      return Integer.valueOf(count);
     }
+  }
+
+  private HColumn<Long,Long> buildColumnFor(int colName) {
+    HColumn<Long,Long> column = HFactory.createColumn(tutorialKeyspace.createClock(), RandomUtils.nextLong(),
+      LongSerializer.get(), LongSerializer.get());
+    return column;
   }
 }
